adf8401c690b00ac93de6913b415c29dca509f4a
CoderJava <kolonel.yudisetiawan@gmail.com>
2020-11-02 00:08:01 +0700

    Change name and day name

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function () {
         new Clock(clocks[i]);
     }
 
-    const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+    const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
     const date = new Date();
     const currHour = date.getHours();
